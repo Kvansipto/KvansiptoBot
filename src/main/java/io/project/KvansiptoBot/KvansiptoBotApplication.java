dfc3c16542e2c59331368b265f4e17c6451c82b3
Kvansipto <Kvansipto@gmail.com>
2024-05-02 12:33:27 +0200

    init project

---
+++ src/main/java/io/project/KvansiptoBot/KvansiptoBotApplication.java
@@ -0,0 +1,13 @@
+package io.project.KvansiptoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvansiptoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvansiptoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/project/KvansiptoBot/KvansiptoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.KvansiptoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvansiptoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
