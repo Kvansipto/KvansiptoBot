17975d41be52e6975abad88d7a87fa8c96c5b5d0
Kvansipto <Kvansipto@gmail.com>
2024-05-07 11:49:42 +0200

    refactoring

--- src/main/java/io/project/KvansiptoBot/service/TelegramBot.java
+++ src/main/java/io/project/KvansiptoBot/service/TelegramBot.java
@@ -10,12 +10,14 @@ import io.project.KvansiptoBot.model.MuscleCommand;
 import io.project.KvansiptoBot.model.MuscleGroup;
 import io.project.KvansiptoBot.model.User;
 import io.project.KvansiptoBot.model.UserRepository;
+import jakarta.annotation.PostConstruct;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
@@ -51,8 +53,10 @@ public class TelegramBot extends TelegramLongPollingBot {
   @Autowired
   private CommandFactory commandFactory;
 
-  public static final String YES_BUTTON = "YES_BUTTON";
-  public static final String NO_BUTTON = "NO_BUTTON";
+  private final Map<String, Consumer<Update>> commands = new HashMap<>();
+  private final Map<String, MuscleGroup> muscleGroupMap;
+  private final Map<String, Exercise> exerciseMap = new HashMap<>();
+
   static final String HELP_TEXT = "This bot was made by Kvansipto\n\n"
       + "You can execute command from the main menu on the left or by typing a command:\n\n"
       + "Type /start to see welcome message\n\n"
@@ -64,15 +68,23 @@ public class TelegramBot extends TelegramLongPollingBot {
     List<BotCommand> commandList = new ArrayList<>();
     commandList.add(new BotCommand("/start", "get welcome message"));
     commandList.add(new BotCommand("/exerciseinfo", "get exercise info"));
-    commandList.add(new BotCommand("/register", "register))"));
-//    commandList.add(new BotCommand("/deletedata", "delete my data"));
     commandList.add(new BotCommand("/help", "how to use this bot"));
-//    commandList.add(new BotCommand("/settings", "set your preferences"));
+    muscleGroupMap = Arrays.stream(MuscleGroup.values()).collect(Collectors.toMap(MuscleGroup::getName, m -> m));
     try {
       this.execute(new SetMyCommands(commandList, new BotCommandScopeDefault(), null));
     } catch (TelegramApiException e) {
       e.printStackTrace();
     }
+  }
+
+  @PostConstruct
+  public void init() {
+    exerciseRepository.findAll().forEach(exercise -> exerciseMap.put(exercise.getName(), exercise));
+    commands.put("/start", this::handleStartCommand);
+    commands.put("/exerciseinfo", this::handleExerciseInfo);
+    commands.put("/help", this::handleHelpCommand);
+    muscleGroupMap.forEach((k, v) -> commands.put(k, this::handleMuscleCommand));
+    exerciseMap.forEach((k, v) -> commands.put(k, this::handleExerciseCommand));
   }
 
   @Override
@@ -81,7 +93,7 @@ public class TelegramBot extends TelegramLongPollingBot {
   }
 
   @EventListener
-  public void handleCommandEvent(MuscleCommandEvent muscleCommandEvent) {
+  public void handleMuscleCommandEvent(MuscleCommandEvent muscleCommandEvent) {
     System.out.println("Called handleCommandEvent");
     SendMessage message = new SendMessage();
     message.setChatId(muscleCommandEvent.getChatId());
@@ -94,81 +106,73 @@ public class TelegramBot extends TelegramLongPollingBot {
 
   @Override
   public void onUpdateReceived(Update update) {
+    Consumer<Update> command = null;
 
     if (update.hasMessage() && update.getMessage().hasText()) {
-      String message = update.getMessage().getText();
-      long chatId = update.getMessage().getChatId();
-
-//      if (message.startsWith("/send") && config.getOwnerChatId().equals(chatId)) {
-//        var textToSend = EmojiParser.parseToUnicode(message.substring(message.indexOf(" ")));
-//        var users = userRepository.findAll();
-//        for (User user : users) {
-//          sendMessage(user.getChatId(), textToSend);
-//        }
-//      } else {
-      switch (message) {
-        case "/start":
-          registerUser(update.getMessage());
-          startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-          break;
-        case "/help":
-          sendMessage(chatId, HELP_TEXT);
-          break;
-        case "/register":
-          register(chatId);
-          break;
-        case "/exerciseinfo":
-          InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-          List<List<InlineKeyboardButton>> rows = new ArrayList<>();
-          List<InlineKeyboardButton> row = new ArrayList<>();
-
-          var muscles = MuscleGroup.values();
-          for (int i = 0; i < muscles.length; i++) {
-            var button = new InlineKeyboardButton();
-            String name = muscles[i].getName();
-            button.setText(name);
-            button.setCallbackData(name);
-            row.add(button);
-            if ((i + 1) % 2 == 0) {
-              rows.add(row);
-              row = new ArrayList<>();
-            }
-          }
-          inlineKeyboardMarkup.setKeyboard(rows);
-          sendMessage(chatId, "Выберите группу мышц", inlineKeyboardMarkup);
-          break;
-        default:
-          sendMessage(chatId, "Sorry, command wasn't recognized");
-      }
+      command = commands.get(update.getMessage().getText());
     } else if (update.hasCallbackQuery()) {
-      String callbackQuery = update.getCallbackQuery().getData();
-      int messageId = update.getCallbackQuery().getMessage().getMessageId();
+      command = commands.get(update.getCallbackQuery().getData());
+    }
+    if (command != null) {
+      command.accept(update);
+    } else {
       long chatId = update.getCallbackQuery().getMessage().getChatId();
-      Map<String, MuscleGroup> muscleGroupMap =
-          Arrays.stream(MuscleGroup.values()).collect(Collectors.toMap(MuscleGroup::getName, m -> m));
-      Map<String, Exercise> exerciseMap = new HashMap<>();
-      var exercises = exerciseRepository.findAll();
-      exercises.forEach(exercise -> exerciseMap.put(exercise.getName(), exercise));
-      if (callbackQuery.equals(YES_BUTTON)) {
-        String text = "You pressed YES button";
-        executeEditMessage(chatId, text, messageId);
-      } else if (callbackQuery.equals(NO_BUTTON)) {
-        String text = "You pressed NO button";
-        executeEditMessage(chatId, text, messageId);
-      } else if (muscleGroupMap.containsKey(callbackQuery)) {
-        MuscleCommand muscleCommand = commandFactory.createMuscleCommand(muscleGroupMap.get(callbackQuery));
-        muscleCommand.execute(chatId);
-      } else if (exerciseMap.containsKey(callbackQuery)) {
-        Exercise exercise = exerciseMap.get(callbackQuery);
-        SendPhoto sendPhoto = new SendPhoto();
-        sendPhoto.setChatId(chatId);
-        sendPhoto.setPhoto(new InputFile(exercise.getImageUrl()));
-        sendPhoto.setCaption(exercise.getDescription());
-        executeTelegramAction(sendPhoto);
-        sendMessage(chatId, String.format("Посмотрите видео с упражнением на YouTube: [Смотреть видео](%s)",
-            exercise.getVideoUrl()));
+      sendMessage(chatId, "Sorry, command wasn't recognized");
+    }
+  }
+
+  private void handleMuscleCommand(Update update) {
+    MuscleCommand muscleCommand = commandFactory
+        .createMuscleCommand(muscleGroupMap.get(update.getCallbackQuery().getData()));
+    muscleCommand.execute(update.getCallbackQuery().getMessage().getChatId());
+  }
+
+  private void handleExerciseCommand(Update update) {
+    String callbackQuery = update.getCallbackQuery().getData();
+    long chatId = update.getCallbackQuery().getMessage().getChatId();
+    Exercise exercise = exerciseMap.get(callbackQuery);
+    SendPhoto sendPhoto = new SendPhoto();
+    sendPhoto.setChatId(chatId);
+    sendPhoto.setPhoto(new InputFile(exercise.getImageUrl()));
+    sendPhoto.setCaption(exercise.getDescription());
+    executeTelegramAction(sendPhoto);
+    sendMessage(chatId, String.format("Посмотрите видео с упражнением на YouTube: [Смотреть видео](%s)",
+        exercise.getVideoUrl()));
+  }
+
+  private void handleStartCommand(Update update) {
+    var message = update.getMessage();
+    registerUser(message);
+    startCommandReceived(message.getChatId(), update.getMessage().getChat().getFirstName());
+  }
+
+  private void handleHelpCommand(Update update) {
+    sendMessage(update.getMessage().getChatId(), HELP_TEXT);
+  }
+
+  private void handleExerciseInfo(Update update) {
+    generateMuscleGroupButtons(update.getMessage().getChatId());
+  }
+
+  private void generateMuscleGroupButtons(long chatId) {
+    InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+    List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+    List<InlineKeyboardButton> row = new ArrayList<>();
+
+    var muscles = MuscleGroup.values();
+    for (int i = 0; i < muscles.length; i++) {
+      var button = new InlineKeyboardButton();
+      String name = muscles[i].getName();
+      button.setText(name);
+      button.setCallbackData(name);
+      row.add(button);
+      if ((i + 1) % 2 == 0) {
+        rows.add(row);
+        row = new ArrayList<>();
       }
     }
+    inlineKeyboardMarkup.setKeyboard(rows);
+    sendMessage(chatId, "Выберите группу мышц", inlineKeyboardMarkup);
   }
 
   private void executeEditMessage(long chatId, String text, int messageId) {
@@ -177,33 +181,6 @@ public class TelegramBot extends TelegramLongPollingBot {
     editMessageText.setText(text);
     editMessageText.setMessageId(messageId);
     executeTelegramAction(editMessageText);
-  }
-
-  private void register(long chatId) {
-
-    SendMessage message = new SendMessage();
-    message.setChatId(chatId);
-    message.setText("Do you really want to register?");
-
-    InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-    List<List<InlineKeyboardButton>> rows = new ArrayList<>();
-    List<InlineKeyboardButton> row = new ArrayList<>();
-
-    InlineKeyboardButton yesButton = new InlineKeyboardButton();
-    yesButton.setText("Yes");
-    yesButton.setCallbackData(YES_BUTTON);
-
-    InlineKeyboardButton noButton = new InlineKeyboardButton();
-    noButton.setText("No");
-    noButton.setCallbackData(NO_BUTTON);
-
-    row.add(yesButton);
-    row.add(noButton);
-    rows.add(row);
-    inlineKeyboardMarkup.setKeyboard(rows);
-    message.setReplyMarkup(inlineKeyboardMarkup);
-
-    executeMessage(message);
   }
 
   private void registerUser(Message message) {
