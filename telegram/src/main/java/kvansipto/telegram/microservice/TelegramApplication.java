65810e90f93a325600fd173b82d0820cf2aaa8b2
Kvansipto <Kvansipto@gmail.com>
2024-06-03 09:42:54 +0200

    fix

---
+++ telegram/src/main/java/kvansipto/telegram/microservice/TelegramApplication.java
@@ -0,0 +1,19 @@
+package kvansipto.telegram.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class TelegramApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TelegramApplication.class, args);
+  }
+
+  @Bean
+  public RestTemplate getRestTemplate() {
+    return new RestTemplate();
+  }
+}
