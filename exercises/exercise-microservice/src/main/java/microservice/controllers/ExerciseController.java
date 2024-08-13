f1efc3928b0c9b3bb5791f65ec3b1733d98e178d
Kvansipto <Kvansipto@gmail.com>
2024-08-13 10:28:19 +0200

    test

--- exercises/exercise-microservice/src/main/java/microservice/controllers/ExerciseController.java
+++ exercises/exercise-microservice/src/main/java/microservice/controllers/ExerciseController.java
@@ -6,6 +6,8 @@ import kvansipto.exercise.dto.ExerciseDto;
 import kvansipto.exercise.resources.ExerciseApi;
 import microservice.entity.MuscleGroup;
 import microservice.service.ExerciseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,13 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExerciseController implements ExerciseApi {
 
+  private static final Logger log = LoggerFactory.getLogger(ExerciseController.class);
+
   @Autowired
   ExerciseService service;
 
   @Override
   @GetMapping("/exercises")
   public List<ExerciseDto> getExercisesByMuscleGroup(@RequestParam("muscleGroup") String muscleGroup) {
-    System.out.println("123256 Получение упражнений для группы мышц: " + muscleGroup);
+    log.info("Получение упражнений для группы мышц: {}" + muscleGroup);
+//    System.out.println("123256 Получение упражнений для группы мышц: " + muscleGroup);
     return service.getExercisesByMuscleGroup(muscleGroup);
   }
 
