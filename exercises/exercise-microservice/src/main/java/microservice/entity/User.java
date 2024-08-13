b52560279d868d13e61d76920eaf76af33b74b35
Kvansipto <Kvansipto@gmail.com>
2024-08-13 13:33:58 +0200

    fix startCommand

--- exercises/exercise-microservice/src/main/java/microservice/entity/User.java
+++ exercises/exercise-microservice/src/main/java/microservice/entity/User.java
@@ -1,29 +1,27 @@
 package microservice.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.io.Serializable;
 import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
+@Builder
 @Entity(name = "users")
-public class User extends BaseEntity {
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
 
+  @Id
+  private Long id;
   private String userName;
   private String firstName;
   private String lastName;
   private Timestamp registeredAt;
-
-  @Builder
-  public User(Long id, String userName, String firstName, String lastName, Timestamp registeredAt) {
-    super(id);
-    this.userName = userName;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.registeredAt = registeredAt;
-  }
 }
