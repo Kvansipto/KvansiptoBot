97018c27342ad19fd477b4bc56d18a2128c3c077
Kvansipto <Kvansipto@gmail.com>
2024-12-04 18:53:17 +0100

    fixed ExerciseResultServiceTest

--- exercises/exercise-microservice/src/main/java/microservice/repository/ExerciseResultRepository.java
+++ exercises/exercise-microservice/src/main/java/microservice/repository/ExerciseResultRepository.java
@@ -1,9 +1,12 @@
 package microservice.repository;
 
+import java.util.List;
 import microservice.entity.ExerciseResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface ExerciseResultRepository extends JpaRepository<ExerciseResult, Long>,
     QuerydslPredicateExecutor<ExerciseResult> {
+
+  List<ExerciseResult> findAllByExercise_Id(Long id);
 }
